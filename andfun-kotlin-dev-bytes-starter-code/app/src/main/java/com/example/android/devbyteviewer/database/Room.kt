dceff0961aedc3e7c6da47642a32e10a96d9d869
WYC <honiwy@gmail.com>
2019-11-26 08:03:59 -0500

    Exercise 10: Add the VideosDatabase

--- andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,16 +17,30 @@
 
 package com.example.android.devbyteviewer.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
 @Dao
 interface VideoDao{
     @Query("select * from databasevideo")
-    fun getVideos(): List<DatabaseVideo>
+    fun getVideos(): LiveData<List<DatabaseVideo>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos:DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version =1)
+abstract class VideoDatabase: RoomDatabase(){
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideoDatabase
+
+fun getDatabase(context: Context): VideoDatabase{
+    if(!::INSTANCE.isInitialized) {
+        INSTANCE=Room.databaseBuilder(context.applicationContext,
+                VideoDatabase::class.java,"videos").build()
+    }
+        return INSTANCE
 }
